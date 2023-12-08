17ee205dce6b16fa1a778d5ceca76618a1036710
Roland Rodriguez <rrrodzila@proton.me>
2023-12-07 22:53:54 -0600

    feat: Add layout, not-found, and page components
    - Added a new file `layout.tsx` which exports a default function component `RootLayout`. This component sets up the basic structure of the HTML document and applies some CSS classes.
    - Added a new file `not-found.tsx` which exports a default function component `NotFound`. This component displays a 404 error message with a button to go back home.
    - Added a new file `page.tsx` which exports a default function component `Home`. This component includes various components such as Hero, CallToAction, Faqs, Footer, etc.
    
    These changes introduce reusable components for layout, handling 404 errors, and creating the main page structure.

---
+++ src/app/layout.tsx
@@ -0,0 +1,45 @@
+import { Inter, Lexend } from 'next/font/google'
+import clsx from 'clsx'
+
+import '@/styles/tailwind.css'
+import { type Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Cruising Life, Inc.',
+    default: 'Cruising Life, Inc. - Sun crafted software',
+  },
+  description:
+    'Software for eco-conscious individuals and businesses seeking efficient, user-friendly technology that aligns with their commitment to sustainability.',
+}
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+})
+
+const lexend = Lexend({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-lexend',
+})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html
+      lang="en"
+      className={clsx(
+        'h-full scroll-smooth bg-white antialiased',
+        inter.variable,
+        lexend.variable,
+      )}
+    >
+      <body className="flex h-full flex-col">{children}</body>
+    </html>
+  )
+}
